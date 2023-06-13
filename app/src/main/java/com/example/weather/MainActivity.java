4da13b8a48c41461b82dc8a5966e34b256e3ae9b
queenyxies <queenycasaclang@gmail.com>
2023-06-13 21:55:10 +0800

    Second Version Commit

--- app/src/main/java/com/example/weather/MainActivity.java
+++ app/src/main/java/com/example/weather/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.weather;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.view.MotionEvent;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -24,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
 
     EditText et_city;
     TextView tv_temp, tv_minTemp, tv_maxTemp, tv_humid, tv_weather, tv_des, tv_date;
-    Button btn_check, btn_clear;
+    Button btn_clear;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         tv_date.setText(currentDate);
 
     }
+
     private void initialize() {
         et_city = findViewById(R.id.et_city);
         tv_temp = findViewById(R.id.tv_temp);
@@ -48,53 +52,36 @@ public class MainActivity extends AppCompatActivity {
         tv_humid = findViewById(R.id.tv_humid);
         tv_weather = findViewById(R.id.tv_weather);
         tv_des = findViewById(R.id.tv_des);
-        btn_check = findViewById(R.id.btn_check);
         btn_clear = findViewById(R.id.btn_clear);
         tv_date = findViewById(R.id.tv_date);
+
+        // Add location and search icons to the EditText
+        Drawable locationIcon = getResources().getDrawable(R.drawable.location_ic);
+        Drawable searchIcon = getResources().getDrawable(R.drawable.search_ic);
+        et_city.setCompoundDrawablesRelativeWithIntrinsicBounds(locationIcon, null, searchIcon, null);
     }
 
 
+    private double convertToFahrenheit(double tempF) {
+        return (tempF - 32) * 5 / 9;
+    }
+
 
     private void function() {
-        btn_check.setOnClickListener(v -> {
-            String city = et_city.getText().toString();
-            String appid = "b5b3a5b2cd1493ce23ec5bc17dde1aba";
-            String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + appid;
+        et_city.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                int DRAWABLE_RIGHT = 2;
 
-            RequestQueue q = Volley.newRequestQueue(this);
-            q.start();
-
-            JsonObjectRequest data = new JsonObjectRequest(Request.Method.GET, url, null,
-                    response -> {
-                        try {
-                            JSONObject obj = response.getJSONObject("main");
-                            JSONArray array = response.getJSONArray("weather");
-                            JSONObject o = array.getJSONObject(0);
-
-                            String temp = obj.getString("temp");
-                            String min = obj.getString("temp_min");
-                            String max = obj.getString("temp_max");
-                            String humid = obj.getString("humidity");
-
-                            String weather = o.getString("main");
-                            String desc = o.getString("description");
-
-                            tv_temp.setText(temp);
-                            tv_minTemp.setText(min);
-                            tv_maxTemp.setText(max);
-                            tv_humid.setText(humid);
-                            tv_weather.setText(weather);
-                            tv_des.setText(desc);
-                        } catch (JSONException e) {
-                            throw new RuntimeException(e);
-                        }
-
-                    },
-                    error -> {
-                        Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
-                    });
-
-            q.add(data);
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (event.getRawX() >= (et_city.getRight() - et_city.getCompoundDrawables()[DRAWABLE_RIGHT].getBounds().width())) {
+                        // Handle search button click here
+                        performSearch();
+                        return true;
+                    }
+                }
+                return false;
+            }
         });
 
         btn_clear.setOnClickListener(v -> {
@@ -107,6 +94,44 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void performSearch() {
+        String city = et_city.getText().toString();
+        String appid = "b5b3a5b2cd1493ce23ec5bc17dde1aba";
+        String url = "https://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + appid;
 
+        RequestQueue q = Volley.newRequestQueue(this);
+        q.start();
 
+        JsonObjectRequest data = new JsonObjectRequest(Request.Method.GET, url, null,
+                response -> {
+                    try {
+                        JSONObject obj = response.getJSONObject("main");
+                        JSONArray array = response.getJSONArray("weather");
+                        JSONObject o = array.getJSONObject(0);
+
+                        String temp = obj.getString("temp");
+                        String min = obj.getString("temp_min");
+                        String max = obj.getString("temp_max");
+                        String humid = obj.getString("humidity");
+
+                        String weather = o.getString("main");
+                        String desc = o.getString("description");
+
+                        tv_temp.setText(temp);
+                        tv_minTemp.setText(min);
+                        tv_maxTemp.setText(max);
+                        tv_humid.setText(humid + "%");
+                        tv_weather.setText(weather);
+                        tv_des.setText(desc);
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+
+                },
+                error -> {
+                    Toast.makeText(this, "Error", Toast.LENGTH_SHORT).show();
+                });
+
+        q.add(data);
+    }
 }
